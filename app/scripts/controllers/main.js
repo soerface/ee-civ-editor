8968b15434e65f394072a0524fe1cbe0745d7624
Soeren Wegener <wegener92@gmail.com>
2015-10-11 23:02:12 +0200

    Basic functionality.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,24 @@
  * Controller of the eeCivEditorApp
  */
 angular.module('eeCivEditorApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.currentGame = 'ee'; // or 'aoe'
+    $scope.selectedBonuses = {};
+    $http({method: 'GET', url: '/civ_info.json'}).success(function(data, status, headers, config) {
+      $scope.civInfo = data;
+    });
+    $scope.toggleAvailableCategory = function(category) {
+      $scope.civInfo['categories'][category].show = !$scope.civInfo['categories'][category].show
+    };
+    $scope.addBonus = function(category, bonus) {
+      $scope.selectedBonuses[category] = $scope.selectedBonuses[category] || [];
+      $scope.selectedBonuses[category].push(bonus);
+    };
+    $scope.removeBonus = function(category, bonus) {
+      var index = $scope.selectedBonuses[category].indexOf(bonus);
+      $scope.selectedBonuses[category].splice(index, 1);
+      if ($scope.selectedBonuses[category].length === 0) {
+        delete $scope.selectedBonuses[category];
+      }
+    };
   });
