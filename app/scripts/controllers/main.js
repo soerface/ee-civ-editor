730b2ea499dad9aef4b9225f2c2d377613d67ca0
Soeren Wegener <wegener92@gmail.com>
2015-10-12 01:18:11 +0200

    Added fancy dancy animations.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,12 +8,26 @@
  * Controller of the eeCivEditorApp
  */
 angular.module('eeCivEditorApp')
-  .controller('MainCtrl', function ($scope, $http) {
-    $scope.currentGame = 'ee'; // or 'aoe'
+  .controller('MainCtrl', function ($scope, $http, $timeout) {
     $scope.selectedBonuses = {};
+    $scope.civilizationName = '';
     $http({method: 'GET', url: '/civ_info.json'}).success(function(data, status, headers, config) {
       $scope.civInfo = data;
     });
+    $scope.selectGame = function(game) {
+      $scope.currentGame = game;
+      var text = 'Unnamed Civilization';
+      var originalLength = text.length;
+      function typingAnimation() {
+        $scope.civilizationName = $scope.civilizationName + text[0];
+        text = text.slice(1, text.length);
+        if (text.length > 0 && $scope.civilizationName.length + text.length == originalLength) {
+          $timeout(typingAnimation, 50 + Math.random() * 300);
+        }
+        $scope.focusCivName = true;
+      }
+      $timeout(typingAnimation, 1000);
+    };
     $scope.toggleAvailableCategory = function(category) {
       $scope.civInfo['categories'][category].show = !$scope.civInfo['categories'][category].show
     };
@@ -26,6 +40,19 @@ angular.module('eeCivEditorApp')
       $scope.selectedBonuses[category].splice(index, 1);
       if ($scope.selectedBonuses[category].length === 0) {
         delete $scope.selectedBonuses[category];
+      }
+    };
+  })
+  .directive('focusMe', function() {
+    return {
+      link: function(scope, element, attrs) {
+        scope.$watch(attrs.focusMe, function(value) {
+          if(value === true) {
+            console.log('value=',value);
+            element[0].focus();
+            scope[attrs.focusMe] = false;
+          }
+        });
       }
     };
   });
