c8a308a8d18bea4c05c11abc284b53f02efcabe2
Soeren Wegener <wegener92@gmail.com>
2015-10-11 16:43:31 +0200

    yo angular.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name eeCivEditorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the eeCivEditorApp
+ */
+angular.module('eeCivEditorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
